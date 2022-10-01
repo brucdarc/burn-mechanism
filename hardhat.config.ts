e6933827e9d28b3ac533d7b18967c30099442e43
DakBuk <brucdarc@gmail.com>
2022-10-01 12:25:34 -0600

    setup hardhat env

---
+++ hardhat.config.ts
@@ -0,0 +1,41 @@
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
+require("hardhat-contract-sizer");
+require('dotenv').config()
+
+const HARMONY_PRIVATE_KEY = process.env.HARMONY_PRIVATE_KEY || '';
+
+module.exports = {
+  solidity: {
+    version: "0.8.17",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+    contractSizer: {
+      alphaSort: true,
+      runOnCompile: true,
+      disambiguatePaths: false,
+    },
+  },
+  networks: {
+    testnet: {
+      url: `https://api.s0.b.hmny.io`,
+      accounts: [`0x${HARMONY_PRIVATE_KEY}`]
+    },
+    mainnet: {
+      url: `https://harmony-0-rpc.gateway.pokt.network`,
+      accounts: [`0x${HARMONY_PRIVATE_KEY}`]
+    }
+  },
+  etherscan: {
+    apiKey: {
+      harmony: 'your API key'
+    }
+  }
+};
